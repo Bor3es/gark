6ff69d075583586640cc19f7407f6bb80237fe49
Boris Y. Ashcheulov <bya1@tpu.ru>
2024-11-29 13:39:35 +0700

    Добавил файлы

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+    unsigned int X,n;
+          
+    cin >> X;
+    cin >> n;
+
+    unsigned int bitValue = (X >> n) & 1;
+     cout << bitValue << endl;
+}
